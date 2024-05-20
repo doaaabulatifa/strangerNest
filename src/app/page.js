6acfadc501f1e48f3ab967356b1c2d7e5375f745
doaaabulatifa <doaalatifa1@gmail.com>
2024-05-20 03:37:02 +0100

    test

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,8 @@ export default function Home() {
     <div>
       <h1>StrangerNest</h1>
       <h2>Welcome in your nest</h2>
-      <p>Welcome to StrangerHub, a unique platform where you can share your most intriguing experiences and strangest tales anonymously with fellow users who go by the name of 'Strangers.' Dive into a world of shared adventures, mysterious encounters,
+      <p>Welcome to StrangerHub, a unique platform where you can share your most intriguing experiences and strangest tales anonymously with fellow users who go by the name of Strangers.
+        Dive into a world of shared adventures, mysterious encounters,
         and unexpected twists,
         all while connecting with others who share your curiosity for the unknown.</p>
         <Image className="image"  src={stra} alt="strange image"   priority={true} />
