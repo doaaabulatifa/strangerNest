e956c7341a352d56a7d541de7c42ae82533ac4bf
doaaabulatifa <doaalatifa1@gmail.com>
2024-05-28 12:12:01 +0100

    undo

--- src/app/myProfile/page.jsx
+++ src/app/myProfile/page.jsx
@@ -2,33 +2,28 @@ import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
-import BasicSelect from "../component/BasicSelect";  // Adjust the path if necessary
-import { useState } from "react";
-import BasicSelect from "../componant/BaiscSelect";
+// import BasicSelect from "../componant/BaiscSelect";
 
 export default async function myProfile() {
-  const { userId } = auth();
-  console.log({userId});
-  const result = await db.query("SELECT * FROM profiles WHERE clerk_id = $1", [userId]);
+    const { userId } = auth();
+    console.log({userId});
+    const result = await db.query("SELECT * FROM profiles WHERE clerk_id = $1", [userId]);
+ 
+  
   const profile = result.rows[0]; 
-
-  const [location, setLocation] = useState(profile.location);
-
-  const handleChange = (event) => {
-    setLocation(event.target.value);
-  };
 
   async function editProfile(formData) {
     "use server";
     const user_name = formData.get("user_name");
-    const email = formData.get("email");
-    const bio = formData.get("bio");
-  
+    const email= formData.get("email");
+    const bio= formData.get("bio");
+    const location= formData.get("location");
     await db.query(
-      "UPDATE profiles SET user_name = $1, email = $2, bio = $3, location = $4 WHERE clerk_id = $5",
-      [user_name, email, bio, location, userId]
-    );
+        "UPDATE profiles SET user_name = $1, email = $2, bio = $3, location = $4 WHERE clerk_id = $5",
+        [user_name, email, bio, location, userId]
+      );
     revalidatePath("/");
+  
     redirect("/");
   }
 
@@ -38,22 +33,30 @@ export default async function myProfile() {
         <label>Name</label>
         <input
           name="user_name"
-          placeholder="Your Name"
+          placeholder="your Name"
           defaultValue={profile.user_name}
         />
         <input
           name="email"
-          placeholder="Your Email"
+          placeholder="your email"
           defaultValue={profile.email}
+         
         />
-        <BasicSelect value={profile.location} onChange={handleChange} />
-        <label>Bio</label>
+        <input
+          name="location"
+          placeholder="your location"
+          defaultValue={profile.location}
+        />
+        {/* <BasicSelect /> */}
+
+        <label>bio</label>
         <textarea
           name="bio"
-          placeholder="Bio"
+          placeholder="bio"
           rows={5}
           defaultValue={profile.bio}
         ></textarea>
+
         <button>Submit Changes</button>
       </form>
     </div>
