a33fc983f00226a0d1fdc6e0ed88694a5fd30fca
doaaabulatifa <doaalatifa1@gmail.com>
2024-05-28 12:27:32 +0100

    test

--- src/app/componant/BaiscSelect.jsx
+++ src/app/componant/BaiscSelect.jsx
@@ -1,33 +1,33 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import InputLabel from '@mui/material/InputLabel';
+// import MenuItem from '@mui/material/MenuItem';
+// import FormControl from '@mui/material/FormControl';
+// import Select from '@mui/material/Select';
 
-export default function BasicSelect({ value, onChange }) {
-  return (
-    <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="location-select-label">Location</InputLabel>
-        <Select
-          labelId="location-select-label"
-          id="location-select"
-          value={value}
-          label="Location"
-          onChange={onChange}
-        >
-          <MenuItem value="uk">UK</MenuItem>
-          <MenuItem value="us">US</MenuItem>
-          <MenuItem value="canada">Canada</MenuItem>
-          <MenuItem value="sweden">Sweden</MenuItem>
-          <MenuItem value="denmark">Denmark</MenuItem>
-          <MenuItem value="uae">UAE</MenuItem>
-          <MenuItem value="jordan">Jordan</MenuItem>
-          <MenuItem value="norway">Norway</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
-  );
-}
+// export default function BasicSelect({ value, onChange }) {
+//   return (
+//     <Box sx={{ minWidth: 120 }}>
+//       <FormControl fullWidth>
+//         <InputLabel id="location-select-label">Location</InputLabel>
+//         <Select
+//           labelId="location-select-label"
+//           id="location-select"
+//           value={value}
+//           label="Location"
+//           onChange={onChange}
+//         >
+//           <MenuItem value="uk">UK</MenuItem>
+//           <MenuItem value="us">US</MenuItem>
+//           <MenuItem value="canada">Canada</MenuItem>
+//           <MenuItem value="sweden">Sweden</MenuItem>
+//           <MenuItem value="denmark">Denmark</MenuItem>
+//           <MenuItem value="uae">UAE</MenuItem>
+//           <MenuItem value="jordan">Jordan</MenuItem>
+//           <MenuItem value="norway">Norway</MenuItem>
+//         </Select>
+//       </FormControl>
+//     </Box>
+//   );
+// }
 
